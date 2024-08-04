d7b62eff4913cc9d49206a809169326b79abe44b
kawakaze <kawakaze_akii@outlook.com>
2024-08-04 23:40:04 +0800

    commit

---
+++ build/CMakeFiles/MyExecutable.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Program/stm32/output/UART/Core/Src/dma.c" "CMakeFiles/MyExecutable.dir/Src/dma.c.obj" "gcc" "CMakeFiles/MyExecutable.dir/Src/dma.c.obj.d"
+  "E:/Program/stm32/output/UART/Core/Src/gpio.c" "CMakeFiles/MyExecutable.dir/Src/gpio.c.obj" "gcc" "CMakeFiles/MyExecutable.dir/Src/gpio.c.obj.d"
+  "E:/Program/stm32/output/UART/Core/Src/main.c" "CMakeFiles/MyExecutable.dir/Src/main.c.obj" "gcc" "CMakeFiles/MyExecutable.dir/Src/main.c.obj.d"
+  "E:/Program/stm32/output/UART/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/MyExecutable.dir/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/MyExecutable.dir/Src/stm32f1xx_hal_msp.c.obj.d"
+  "E:/Program/stm32/output/UART/Core/Src/stm32f1xx_it.c" "CMakeFiles/MyExecutable.dir/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/MyExecutable.dir/Src/stm32f1xx_it.c.obj.d"
+  "E:/Program/stm32/output/UART/Core/Src/system_stm32f1xx.c" "CMakeFiles/MyExecutable.dir/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/MyExecutable.dir/Src/system_stm32f1xx.c.obj.d"
+  "E:/Program/stm32/output/UART/Core/Src/tim.c" "CMakeFiles/MyExecutable.dir/Src/tim.c.obj" "gcc" "CMakeFiles/MyExecutable.dir/Src/tim.c.obj.d"
+  "E:/Program/stm32/output/UART/Core/Src/usart.c" "CMakeFiles/MyExecutable.dir/Src/usart.c.obj" "gcc" "CMakeFiles/MyExecutable.dir/Src/usart.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExecutable.dir/Src/dma.c.obj"
+  "CMakeFiles/MyExecutable.dir/Src/dma.c.obj.d"
+  "CMakeFiles/MyExecutable.dir/Src/gpio.c.obj"
+  "CMakeFiles/MyExecutable.dir/Src/gpio.c.obj.d"
+  "CMakeFiles/MyExecutable.dir/Src/main.c.obj"
+  "CMakeFiles/MyExecutable.dir/Src/main.c.obj.d"
+  "CMakeFiles/MyExecutable.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/MyExecutable.dir/Src/stm32f1xx_hal_msp.c.obj.d"
+  "CMakeFiles/MyExecutable.dir/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/MyExecutable.dir/Src/stm32f1xx_it.c.obj.d"
+  "CMakeFiles/MyExecutable.dir/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/MyExecutable.dir/Src/system_stm32f1xx.c.obj.d"
+  "CMakeFiles/MyExecutable.dir/Src/tim.c.obj"
+  "CMakeFiles/MyExecutable.dir/Src/tim.c.obj.d"
+  "CMakeFiles/MyExecutable.dir/Src/usart.c.obj"
+  "CMakeFiles/MyExecutable.dir/Src/usart.c.obj.d"
+  "MyExecutable.exe"
+  "MyExecutable.exe.manifest"
+  "MyExecutable.pdb"
+  "libMyExecutable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
